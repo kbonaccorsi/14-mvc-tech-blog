982ed282d0f3e4ea3b44dd8c6491d02983097a65
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-11-05 14:45:20 -0500

    working on dashboard-routes.js

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,44 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/login', async (req, res) => {
+    try {
+        //find the user with a specific username
+        const userData = await User.findOne({ where: { email: req.body.username } });
+
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+
+        //verify the posted password with the password stored in the database
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect username or password, please try again' });
+            return;
+        }
+
+        //Create session variables based on the logged in user
+        req.session.save(() => {
+            req.session.user_id = userData.isSoftDeleted;
+            req.session.logged_in = true;
+
+            req.json({ user: userData, message: 'You\'re now logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(400).end();
+    }
+});
+
+module.exports = router;
